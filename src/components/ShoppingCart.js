aa1c68fe0fa4f9a373e049586ae726a0f65a1b7b
RedPho <medasdan@gmail.com>
2023-03-19 20:45:24 +0300

    Make ShoppingCart component dynamic

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,13 @@
+import React, { useEffect, useState } from "react";
+
 export default function ShoppingCart() {
+
+  const [itemsInCart, setItemsInCart] = useState([]);
+  useEffect(()=> {
+    setItemsInCart([{ name: "first item", quantity: "3", image: "" }, { name: "second item", quantity: "2", image: "" }])
+  })
+
+
   return (
     <div className="overflow-x-auto">
       <table className="table w-full">
@@ -12,110 +21,40 @@ export default function ShoppingCart() {
             </th>
             <th>Item</th>
             <th>Quantity</th>
-            <th>remove selected</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {/* row 1 */}
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+          {itemsInCart.map((item) =>
+          (
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">{item.name}</div>
+                    <div className="text-sm opacity-50">item description</div>
                   </div>
                 </div>
-                <div>
-                  <div className="font-bold">Hart Hagerty</div>
-                  <div className="text-sm opacity-50">United States</div>
-                </div>
-              </div>
-            </td>
-            <td>Purple</td>
-            <th>
-              <button className="btn btn-error btn-xs">remove</button>
-            </th>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Brice Swyre</div>
-                  <div className="text-sm opacity-50">China</div>
-                </div>
-              </div>
-            </td>
-            <td>Red</td>
-            <th>
-              <button className="btn btn-error btn-xs">remove</button>
-            </th>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src="/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Marjy Ferencz</div>
-                  <div className="text-sm opacity-50">Russia</div>
-                </div>
-              </div>
-            </td>
-            <td>Crimson</td>
-            <th>
-              <button className="btn btn-error btn-xs">remove</button>
-            </th>
-          </tr>
-          {/* row 4 */}
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Yancy Tear</div>
-                  <div className="text-sm opacity-50">Brazil</div>
-                </div>
-              </div>
-            </td>
-            <td>Indigo</td>
-            <th>
-              <button className="btn btn-error btn-xs">remove</button>
-            </th>
-          </tr>
+              </td>
+              <td>{item.quantity}</td>
+              <th>
+                <button className="btn btn-error btn-xs">remove</button>
+              </th>
+            </tr>
+          )
+          )};
+
         </tbody>
       </table>
     </div>
