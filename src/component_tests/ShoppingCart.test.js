d6a7666dd111cbf709dd2853b95602e1a0280441
RedPho <medasdan@gmail.com>
2023-03-21 19:43:20 +0300

    Clean unused items

--- src/component_tests/ShoppingCart.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ShoppingCart from '../components/ShoppingCart';
-
-test("Renders name, quantity and delete button in the props", ()=> {
-  
-})
